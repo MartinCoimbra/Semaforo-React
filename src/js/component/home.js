a7a5ea50979ab6f79adcb3853bcea43b403af549
MartinCoimbra <martin.coimbra.2017@gmail.com>
2021-04-28 19:49:48 +0000

    Botonse terminados

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -30,19 +30,24 @@ export function Home() {
 		padding: "45px",
 		borderRadius: "100%",
 		background: "red",
-	
+		webkitBoxShadow:
+			"5px 5px 5px 0px #000000, inset 4px 4px 15px 0px #000000, 40px 5px 27px 38px rgba(255, 255, 255, 0)",
+		boxShadow:
+			" 8px 5px 29px 16px red, inset 4px 4px 15px 0px #000000, 40px 5px 27px 38px rgba(255, 255, 255, 0)"
 	};
 	const luz2 = {
 		padding: "45px",
 		borderRadius: "100%",
 		background: "yellow",
-		
+		webkitBoxShadow:
+			"5px 5px 5px 0px #000000, inset 4px 4px 15px 0px #000000, 40px 5px 27px 38px rgba(255, 255, 255, 0)",
+		boxShadow:
+			" 8px 5px 29px 16px yellow, inset 4px 4px 15px 0px #000000, 40px 5px 27px 38px rgba(255, 255, 255, 0)"
 	};
 	const luz3 = {
 		padding: "45px",
 		borderRadius: "100%",
 		background: "green",
-		webkitBoxShadow: "8px 5px 29px 16px #FFFFFF",
 		webkitBoxShadow:
 			"5px 5px 5px 0px #000000, inset 4px 4px 15px 0px #000000, 40px 5px 27px 38px rgba(255, 255, 255, 0)",
 		boxShadow:
@@ -53,20 +58,26 @@ export function Home() {
 	/* Funcion de cambio de color */
 	const cambioColor = () => {
 		setEncendido1(luz1);
+		setEncendido2(botonComun);
+		setEncendido3(botonComun);
 	};
 	const [encendido2, setEncendido2] = useState(botonComun);
 	/* Funcion de cambio de color */
 	const cambioColor2 = () => {
 		setEncendido2(luz2);
+		setEncendido1(botonComun);
+		setEncendido3(botonComun);
 	};
 	const [encendido3, setEncendido3] = useState(botonComun);
 	/* Funcion de cambio de color */
 	const cambioColor3 = () => {
 		setEncendido3(luz3);
+		setEncendido1(botonComun);
+		setEncendido2(botonComun);
 	};
 
 	return (
-		<div className="container">
+		<div className="container ">
 			<div className="row justify-content-center">
 				<div className="bg-dark" style={palito}></div>
 			</div>
