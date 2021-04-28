5c2f72ab112a8f0d037a494ed2111c0332f6ab86
MartinCoimbra <martin.coimbra.2017@gmail.com>
2021-04-28 18:42:31 +0000

    botones dise;o

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,24 +1,38 @@
 import React from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
 //create your first component
 export function Home() {
+	const estructuraSemaforo = {
+		width: "200px",
+		height: "400px"
+	};
+	const palito = {
+		width: "20px",
+		height: "50px"
+	};
+	const luz = {
+		background: "red",
+		padding: "35px",
+		borderRadius: "100%"
+	};
 	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="container">
+			<div className="row justify-content-center">
+				<div className="bg-dark" style={palito}></div>
+			</div>
+			<div className="row justify-content-center text-center">
+				<div className="bg-dark" style={estructuraSemaforo}>
+					<div className="col-12 mt-5 h-25">
+						<button style={luz}>Red</button>
+					</div>
+					<div className="col-12 mt-2 h-25">
+						<button style={luz}>Amarillo</button>
+					</div>
+					<div className="col-12 mt-2 h-25">
+						<button style={luz}>Green</button>
+					</div>
+				</div>
+			</div>
 		</div>
 	);
 }
